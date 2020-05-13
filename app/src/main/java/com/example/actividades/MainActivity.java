b7de373a56c148f77f578b3b8b072817d65e51be
aljofloro <aljofloro@gmail.com>
2020-05-12 21:35:08 -0500

    modificación sobre mensajes

--- app/src/main/java/com/example/actividades/MainActivity.java
+++ app/src/main/java/com/example/actividades/MainActivity.java
@@ -9,9 +9,11 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
     public void acercaDe(View view){
-      Intent intent = new Intent(this,AcercaDeActivity.class);
-      startActivity(intent);
+        Intent intent = new Intent(getBaseContext(),AcercaDeActivity.class);
+        startActivity(intent);
     }
 
     @Override
@@ -19,36 +21,48 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(getBaseContext(),"onCreate()",Toast.LENGTH_SHORT).show();
+        Log.e(LOG_TAG,"onCreate");
     }
 
     @Override
     protected void onStart(){
         super.onStart();
         Toast.makeText(getBaseContext(),"onStart()",Toast.LENGTH_SHORT).show();
+        Log.e(LOG_TAG, "onStart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         Toast.makeText(getBaseContext(),"onResume()",Toast.LENGTH_SHORT).show();
+        Log.e(LOG_TAG,"onResume");
     }
 
     @Override
     protected void onPause(){
         super.onPause();
         Toast.makeText(getBaseContext(),"onPause()",Toast.LENGTH_SHORT).show();
-        Log.e("mensaje","onPause");
+        Log.e(LOG_TAG,"onPause");
     }
 
     @Override
     protected void onStop(){
         super.onStop();
         Toast.makeText(getBaseContext(),"onStop()",Toast.LENGTH_SHORT).show();
+        Log.e(LOG_TAG,"onStop");
     }
 
     @Override
     protected void onRestart(){
         super.onRestart();
         Toast.makeText(getBaseContext(),"onRestart()",Toast.LENGTH_SHORT).show();
+        Log.e(LOG_TAG,"onRestart");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Toast.makeText(getBaseContext(),"onDestroy()",Toast.LENGTH_SHORT).show();
+        Log.e(LOG_TAG,"onDestoy");
     }
 }
